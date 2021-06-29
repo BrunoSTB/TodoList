3497daa4d252b98df87debd400140d792e2418b4
BrunoSTB <brunobragast@gmail.com>
2021-06-29 20:05:20 -0300

    Update: Now able to register and login!

--- content/application.py
+++ content/application.py
@@ -1,11 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
+from flask_session import Session
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
 
+
+# Starts Flask
 app = Flask(__name__)
+app.secret_key = b'\xc8\x95\nj\xd0\x8a\xb6e\xbez\x97\xc6V\x97\xf0\xab'
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+
+        # FORGET USER ID
+        session.clear()
+
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            # CHECK IF BOTH USERNAME AND PASSWORD WERE INPUTED
+            if not request.form.get('username') or not request.form.get('password'):
+                return render_template("login.html")
+
+            # CHECK IF USERNAME EXISTS
+            if not db.execute( "SELECT username FROM users WHERE username = ?", (request.form.get('username'), ) ):
+                return render_template("login.html")
+
+            # CHECK IF PASSWORD MATCHES
+            hashed_pass =  db.execute( "SELECT hashed_pass FROM users WHERE username = ?", (request.form.get('username'), ) ).fetchall()
+            if check_password_hash( hashed_pass[0][0], request.form.get('password') ):
+                session["user_id"] = db.execute( "SELECT id FROM users WHERE username = ?", (request.form.get('username'), ) ).fetchall()[0][0]
+                return redirect("/")
+            
     return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template("login.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            # CHECK IF USERNAME AND PASSWORD WERE INPUTED
+            if not request.form.get('username') or not request.form.get('password'):
+                print("1")
+                return render_template("register.html")
+
+            # CHECK IF USERNAME IS AVAILABLE
+            rows = db.execute("SELECT username FROM users WHERE username = ?", (request.form.get('username'), ) ).fetchall()
+            if len(rows) != 0: 
+                
+                return render_template("register.html")
+
+            # CHECK IF PASSWORDS MATCH
+            elif request.form.get("password") == request.form.get("passwordRepeat"):
+                # CREATE ACCOUNTS
+                db.execute("INSERT INTO users (username, hashed_pass) VALUES (?, ?)", 
+                           (request.form.get('username'), 
+                           generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)) )
+                database.commit()
+                return render_template("login.html")    
+
+    return render_template("register.html")
--- content/static/todolist.js
+++ content/static/todolist.js
@@ -1,38 +1,66 @@
-function new_post_it() {
-    while (true) {
-        var newItem = document.getElementById('user_input').value;
-        if (newItem === null || newItem === "") {
+// Create a new post it
+function new_Post_It() {
+    while ( true ) {
+
+        var content = document.getElementById('user_input').value;
+
+        if (content === null || content === "") {
             break;
         }
-        else if (newItem.length > 80) {
+        else if (content.length > 80) {
             alert("Text is bigger than the allowed!")
             break;
         }
-        document.getElementById("current").innerHTML += "<li class='m-3 card " + random_color() + "'><div class='d-flex justify-content-around m-0 p-0'><button onclick='remove_self(this)' class='btn btn-sm'><b>x</b></button><input class='ml-auto' onclick='togglePalette(this)' id='colorPick' type='checkbox'>" + colorPalette() + "</div><div class='card-body px-1 pb-2 pt-0'>" + newItem + "</div></li>";
+
+
+        // Modifies HTML to add the postit as a <li> card. 
+        document.getElementById("current").innerHTML +=
+            "<li class='m-3 card " + random_Color() + "'>" +
+                "<div class='d-flex justify-content-around m-0 p-0'>" +
+                    "<button onclick='remove_Self(this)' class='btn btn-sm'><b>x</b></button>" +
+                    "<input class='ml-auto' onclick='toggle_Palette(this)' id='colorPick' type='checkbox'>" +
+                    "<div class='m-0 px-1 card' id='palette' >" +
+                        "<i onclick='change_Color(this, 1)' class='bi bi-square-fill d-block'></i>" +
+                        "<i onclick='change_Color(this, 2)' class='bi bi-square-fill d-block'></i>" +
+                        "<i onclick='change_Color(this, 3)' class='bi bi-square-fill d-block'></i>" +
+                        "<i onclick='change_Color(this, 4)' class='bi bi-square-fill d-block'></i>" +
+                    "</div>" + 
+                "</div>" +
+                "<div class='card-body px-1 pb-2 pt-0'>" + 
+                    content +
+                "</div>" +
+            "</li>";   
+
+        // Clears input
         document.getElementById('user_input').value = '';
         break;
     }
 }
 
-function delete_all() {
+// Deletes all post its
+function delete_All() {
     if (confirm('Are you sure you want to delete all Postits?')) {
+        // Deletes the ordered list and replaces it with an empty copy
+        
         var chosen = document.getElementById('current');
         chosen.remove()
         document.getElementById('parag').innerHTML += "<ol class='d-flex flex-row justify-content-center flex-wrap' id='current'></ol>";
     }
 }
 
-function remove_self(el) {
+// Remove a post it
+function remove_Self(el) {
     var element = el.parentElement.parentElement;
     element.remove();
 }
 
-function getPrevious(el) {
-    var element = el.previousSibling;
-    return element;
-}
 
-function togglePalette(el) {
+// Auxiliary Functions ----
+
+// For functionality --
+
+// Toggles the palette menu
+function toggle_Palette(el) {
     var checkbox = el;
     var toggleDiv = el.nextSibling;
     if (checkbox.checked == true) {
@@ -44,8 +72,28 @@ function togglePalette(el) {
     }
 }
 
+// In the palette menu, allows the user to change the post it color
+function change_Color(element, color) {
+    var element = element.parentElement.previousSibling.parentElement.parentElement;
 
-function random_color() {
+    if (color === 1) {
+        element.style.backgroundColor = "#2a9d8f";
+    }
+    else if (color === 2) {
+        element.style.backgroundColor = "#e9c46a";
+    }
+    else if (color === 3) {
+        element.style.backgroundColor = "#f4a261";
+    }
+    else if (color === 4) {
+        element.style.backgroundColor = "#e76f51";
+    }
+}
+
+// For cleaner code --
+
+// Get a random collor (used for random color postit when newly created)
+function random_Color() {
     randomize = Math.floor(Math.random() * (5 - 1)) + 1;
     switch (randomize) {
         case 1:
@@ -59,23 +107,4 @@ function random_color() {
     }
 }
 
-function changeColor(element, color) {
-    var element = element.parentElement.previousSibling.parentElement.parentElement;
 
-    if(color === 1) {
-        element.style.backgroundColor = "#2a9d8f";
-    }
-    else if (color === 2) {
-        element.style.backgroundColor = "#e9c46a";
-    }
-    else if (color ===  3) {
-        element.style.backgroundColor = "#f4a261";
-    }
-    else if (color === 4) {
-        element.style.backgroundColor = "#e76f51";
-    }
-}
-
-function colorPalette() {
-    return "<div class='m-0 px-1 card' id='palette' ><i onclick='changeColor(this, 1)' class='bi bi-square-fill d-block'></i><i onclick='changeColor(this, 2)' class='bi bi-square-fill d-block'></i><i onclick='changeColor(this, 3)' class='bi bi-square-fill d-block'></i><i onclick='changeColor(this, 4)' class='bi bi-square-fill d-block'></i> </div>";
-}
