ec8242a4fcc45d0f2932623662e70d3168e6a658
BrunoSTB <brunobragast@gmail.com>
2021-07-13 17:05:13 -0300

    Login implemented

---
+++ content/app.py
@@ -0,0 +1,193 @@
+from flask import Flask, render_template, session, request, redirect
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+
+# Starts Flask
+app = Flask(__name__)
+app.secret_key = b'\xc8\x95\nj\xd0\x8a\xb6e\xbez\x97\xc6V\x97\xf0\xab'
+
+
+# Decorator
+# https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+##########################
+# Note Management
+
+@app.route('/', methods=["GET", "POST"])
+@login_required
+def index():
+    # Connect to database
+    with sqlite3.connect("accounts.db") as database:
+        db = database.cursor()
+
+        # Get user's post its
+        userNotes = db.execute(
+            "SELECT * FROM notes WHERE userid = ?", (session["user_id"], )).fetchall()
+
+        # Create new post it
+        if request.method == "POST":
+            db.execute("INSERT INTO notes (content, color, userid) VALUES(?,?,?)",
+                       (request.form.get("content"), "#2a9d8f", session["user_id"], ))
+            database.commit()
+            return redirect("/")
+
+        # Render page
+        elif request.method == "GET":
+            return render_template("index.html", userNotes=userNotes)
+
+
+# Remove post it
+@app.route('/remove', methods=["GET", "POST"])
+def remove():
+    # Check for correct method & connect to database
+    if request.method == "POST":
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            # Get current user post its
+            userNotes = db.execute(
+                "SELECT * FROM notes WHERE userid = ?", (session["user_id"], )).fetchall()
+
+            # Search for the post it to be deleted in database, then delete it
+            for row in userNotes:
+                if int(request.form.get('removeBtn')) == int(row[0]):
+                    db.execute("DELETE FROM notes WHERE id = ? AND userid = ?", (str(
+                        row[0]), session['user_id']))
+                    database.commit()
+                    return redirect("/")
+
+    return redirect("/")
+
+
+# Delete All Notes
+@app.route('/deleteAll', methods=["POST"])
+def delete():
+    if request.method == "POST":
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            db.execute("DELETE FROM notes WHERE userid = ?",
+                       (session['user_id'], ))
+            database.commit()
+
+            return redirect("/")
+
+    return redirect("/")
+
+
+# Change Postit Color
+@app.route('/changeColor', methods=["POST"])
+def changeColor():
+    with sqlite3.connect("accounts.db") as database:
+        db = database.cursor()
+
+        userNotes = db.execute(
+            "SELECT * FROM notes WHERE userid = ?", (session["user_id"], )).fetchall()
+
+        # Check which button
+        if request.form.get('blueBtn'):
+            for row in userNotes:
+                if int(request.form.get('blueBtn')) == int(row[0]):
+                    db.execute(
+                        "UPDATE notes SET color = '#2a9d8f' WHERE id = ? AND userid = ?", (str(row[0]), session["user_id"]))
+
+        elif request.form.get('yellowBtn'):
+            for row in userNotes:
+                if int(request.form.get('yellowBtn')) == int(row[0]):
+                    db.execute(
+                        "UPDATE notes SET color = '#e9c46a' WHERE id = ? AND userid = ?", (str(row[0]), session["user_id"]))
+
+        elif request.form.get('orangeBtn'):
+            for row in userNotes:
+                if int(request.form.get('orangeBtn')) == int(row[0]):
+                    db.execute(
+                        "UPDATE notes SET color = '#f4a261' WHERE id = ? AND userid = ?", (str(row[0]), session["user_id"]))
+
+        elif request.form.get('redBtn'):
+            for row in userNotes:
+                if int(request.form.get('redBtn')) == int(row[0]):
+                    db.execute(
+                        "UPDATE notes SET color = '#e76f51' WHERE id = ? AND userid = ?", (str(row[0]), session["user_id"]))
+
+        database.commit()
+        return redirect("/")
+
+
+##########################
+# User management
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        # FORGET USER ID
+        session.clear()
+
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            # CHECK IF BOTH USERNAME AND PASSWORD WERE INPUTED
+            if not request.form.get('username') or not request.form.get('password'):
+                return render_template("login.html")
+
+            # CHECK IF USERNAME EXISTS
+            if not db.execute("SELECT username FROM users WHERE username = ?", (request.form.get('username'), )):
+                return render_template("login.html")
+
+            # CHECK IF PASSWORD MATCHES
+            hashed_pass = db.execute(
+                "SELECT hashed_pass FROM users WHERE username = ?", (request.form.get('username'), )).fetchall()
+            if check_password_hash(hashed_pass[0][0], request.form.get('password')):
+                session["user_id"] = db.execute(
+                    "SELECT id FROM users WHERE username = ?", (request.form.get('username'), )).fetchall()[0][0]
+                return redirect("/")
+
+    return render_template("login.html")
+
+# Log out
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/login")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        with sqlite3.connect("accounts.db") as database:
+            db = database.cursor()
+
+            # CHECK IF USERNAME AND PASSWORD WERE INPUTED
+            if not request.form.get('username') or not request.form.get('password'):
+                print("1")
+                return render_template("register.html")
+
+            # CHECK IF USERNAME IS AVAILABLE
+            rows = db.execute("SELECT username FROM users WHERE username = ?",
+                              (request.form.get('username'), )).fetchall()
+            if len(rows) != 0:
+
+                return render_template("register.html")
+
+            # CHECK IF PASSWORDS MATCH
+            elif request.form.get("password") == request.form.get("passwordRepeat"):
+                # CREATE ACCOUNTS
+                db.execute("INSERT INTO users (username, hashed_pass) VALUES (?, ?)",
+                           (request.form.get('username'),
+                            generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)))
+                database.commit()
+                return render_template("login.html")
+
+    return render_template("register.html")
--- content/static/todolist.js
+++ content/static/todolist.js
@@ -1,68 +1,7 @@
-// Create a new post it
-function new_Post_It() {
-    while ( true ) {
 
-        var content = document.getElementById('user_input').value;
-
-        if (content === null || content === "") {
-            break;
-        }
-        else if (content.length > 80) {
-            alert("Text is bigger than the allowed!")
-            break;
-        }
-
-
-        // Modifies HTML to add the postit as a <li> card. 
-        document.getElementById("current").innerHTML +=
-            "<li class='m-3 card " + random_Color() + "'>" +
-                "<div class='d-flex justify-content-around m-0 p-0'>" +
-                    "<button onclick='remove_Self(this)' class='btn btn-sm'><b>x</b></button>" +
-                    "<input class='ml-auto' onclick='toggle_Palette(this)' id='colorPick' type='checkbox'>" +
-                    "<div class='m-0 px-1 card' id='palette' >" +
-                        "<i onclick='change_Color(this, 1)' class='bi bi-square-fill d-block'></i>" +
-                        "<i onclick='change_Color(this, 2)' class='bi bi-square-fill d-block'></i>" +
-                        "<i onclick='change_Color(this, 3)' class='bi bi-square-fill d-block'></i>" +
-                        "<i onclick='change_Color(this, 4)' class='bi bi-square-fill d-block'></i>" +
-                    "</div>" + 
-                "</div>" +
-                "<div class='card-body px-1 pb-2 pt-0'>" + 
-                    content +
-                "</div>" +
-            "</li>";   
-
-        // Clears input
-        document.getElementById('user_input').value = '';
-        break;
-    }
-}
-
-// Deletes all post its
-function delete_All() {
-    if (confirm('Are you sure you want to delete all Postits?')) {
-        // Deletes the ordered list and replaces it with an empty copy
-        
-        var chosen = document.getElementById('current');
-        chosen.remove()
-        document.getElementById('parag').innerHTML += "<ol class='d-flex flex-row justify-content-center flex-wrap' id='current'></ol>";
-    }
-}
-
-// Remove a post it
-function remove_Self(el) {
-    var element = el.parentElement.parentElement;
-    element.remove();
-}
-
-
-// Auxiliary Functions ----
-
-// For functionality --
-
-// Toggles the palette menu
 function toggle_Palette(el) {
     var checkbox = el;
-    var toggleDiv = el.nextSibling;
+    var toggleDiv = el.nextElementSibling;
     if (checkbox.checked == true) {
         toggleDiv.style.display = 'inline-block';
     }
@@ -71,40 +10,3 @@ function toggle_Palette(el) {
         toggleDiv.style.display = 'none';
     }
 }
-
-// In the palette menu, allows the user to change the post it color
-function change_Color(element, color) {
-    var element = element.parentElement.previousSibling.parentElement.parentElement;
-
-    if (color === 1) {
-        element.style.backgroundColor = "#2a9d8f";
-    }
-    else if (color === 2) {
-        element.style.backgroundColor = "#e9c46a";
-    }
-    else if (color === 3) {
-        element.style.backgroundColor = "#f4a261";
-    }
-    else if (color === 4) {
-        element.style.backgroundColor = "#e76f51";
-    }
-}
-
-// For cleaner code --
-
-// Get a random collor (used for random color postit when newly created)
-function random_Color() {
-    randomize = Math.floor(Math.random() * (5 - 1)) + 1;
-    switch (randomize) {
-        case 1:
-            return "bg-blueish";
-        case 2:
-            return "bg-yellowish";
-        case 3:
-            return "bg-orangeish";
-        case 4:
-            return "bg-redish";
-    }
-}
-
-
